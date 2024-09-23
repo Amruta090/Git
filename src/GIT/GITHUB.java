72f142fdb65f65829518125a2e65edd23d23fc84
AkshayVaidya1234 <akshayvaidya733@gmail.com>
2024-09-23 18:37:22 +0530

    Akshays commit

--- src/GIT/GITHUB.java
+++ src/GIT/GITHUB.java
@@ -5,7 +5,7 @@ public class GITHUB {
 	public static void main(String[] args) {
 		System.out.println("my 1st GITHUB Demo");
 		System.out.println("Amruta");
-
+		System.out.println("Akshay");
 	}
 
 }
